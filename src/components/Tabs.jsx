d9969c409a91ef1a0b4644e511889041ec27e0f2
cami-js <camirivenson@gmail.com>
2024-05-13 12:06:25 -0300

    intento de implementacion de iconos

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 import Home from './src/views/Home';
 import Login from './src/views/Login';
+import { Entypo, MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -29,27 +29,31 @@ function LoginStack() {
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Home"
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          tabBarActiveTintColor: '#e91e63',
+        }}
+      >
+        <Tab.Screen 
+          name="Home" 
           component={HomeStack}
           options={{
             tabBarIcon: ({ color, size }) => (
-              <FontAwesomeIcon icon={faHome} size={size} color={color} />
+              <FontAwesome5 name="home" size={24} color="black" />
             ),
-          }}
+          }} 
         />
-        <Tab.Screen
-          name="Login"
+        <Tab.Screen 
+          name="Login" 
           component={LoginStack}
           options={{
             tabBarIcon: ({ color, size }) => (
-              <FontAwesomeIcon icon={faUser} size={size} color={color} />
+              <MaterialCommunityIcons name="account" color={color} size={size} />
             ),
-          }}
+          }} 
         />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
